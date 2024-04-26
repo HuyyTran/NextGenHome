bb376a9a5a2537d6f63d555a1042bae582d900d9
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2024-04-26 14:26:38 +0700

    feat: add purifier control

---
+++ NextGenHomeFrontEnd/front_end/src/components/PurifierControl/PurifierControl.js
@@ -0,0 +1,105 @@
+import { StyleSheet, View, Text} from 'react-native';
+import * as React from 'react';
+import {Switch} from 'react-native-paper';
+import {useState} from 'react';
+import { Wind } from 'lucide-react-native';
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex:0.5, 
+        height:180, 
+        gap:5,
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'left',
+        backgroundColor: '#282424',
+        borderWidth: 1,
+        borderRadius: 16,
+    },
+    sub_container : {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        
+    },
+    bottom_sub:{
+        width: "90%",
+        flex: 0.6,
+        borderTopWidth: 1,
+        borderTopColor: '#F8F8F855',
+        borderTopStartRadius: 20,
+        borderTopEndRadius: 20
+    },
+    number: {
+        flex: 0.5,
+        fontSize: 25,
+        textAlign: 'left',
+        marginTop: 20,
+        marginHorizontal: 15,
+        color: '#F8F8F8',
+    },
+    sub_text:
+    {
+        opacity: 0.60,
+        fontSize: 12,
+        textAlign: 'left',
+        marginHorizontal: 15,
+        color: '#F8F8F8',
+    },
+    button: {
+        flexDirection: 'column',
+    },
+})
+
+export default function PurifierControl(
+    {deviceName,
+    Temperature,
+    onToggleChange,
+    initialToggleState}
+){
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => {
+        setIsEnabled(previousState => !previousState);
+        if (onToggleChange != undefined)
+        {
+            onToggleChange(!isEnabled);
+        }
+    }
+    if (initialToggleState != undefined)
+    {
+        setIsEnabled(initialToggleState);
+    }
+    if (Temperature == undefined)
+    {
+        Temperature = "10";
+    }
+    return (
+        <View style={styles.container}>
+            <View style={[styles.sub_container, {flex: 1}]}>
+                <View style={{flex: 0.75}}>
+                    <Text style={styles.number}>{Temperature}°C</Text>
+                    <Text style={[styles.sub_text, {flex: 0.3}] }>{deviceName}</Text>
+                    <Text style={[styles.sub_text, {flex: 0.3}] }>Air</Text>
+                </View>
+                <Wind 
+                    style={{flex: 1,}} 
+                    color={"#F8F8F8"}
+                    size={32}
+                ></Wind>
+            </View>
+            
+            <View style={[styles.sub_container, styles.bottom_sub]}>
+                <Text style={[styles.sub_text, {flex: 1}] }>Mode 1</Text>
+                <Switch 
+                    style={styles.button}
+                    trackColor={{false: '#F8F8F8', true: '#FFB267'}}
+                    thumbColor={'#282424'}r
+                    ios_backgroundColor="#F8F8F8"
+                    onValueChange={toggleSwitch}
+                    value={isEnabled}
+                ></Switch>
+            </View>
+        </View>
+    )
+}
