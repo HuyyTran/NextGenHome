078a819e236c47ec4912afe1fdc2e4504eeb8a42
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2024-05-02 22:07:10 +0700

    feat: notification screen

--- NextGenHomeFrontEnd/front_end/src/screen/Notification/Notification.js
+++ NextGenHomeFrontEnd/front_end/src/screen/Notification/Notification.js
@@ -1,7 +1,11 @@
-import { StyleSheet, View, Text, Modal, Button} from 'react-native';
+import { StyleSheet, View, Text, Modal, Button, ScrollView} from 'react-native';
 import {useState, useRef} from 'react';
 import Header from '../Header/Header';
-
+import { selectNotificationList } from '../../helper/globalState/GlobalState';
+import { useSelector } from 'react-redux';
+import NotificationItem from '../../components/NotificationItem/NotificationItem';
+import { removeFromNotificationListByIndex } from '../../helper/globalState/GlobalState';
+import store from '../../helper/ReduxStore';
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -14,22 +18,55 @@ const styles = StyleSheet.create({
         paddingLeft: 20,
         paddingRight: 20,
     },
+    subcontainer: {
+        flex: 1,
+        marginTop: 20,
+        backgroundColor: 'transparent',
+        flexDirection: 'column',
+        borderColor: '#fff',
+      },
     title: {
-    fontSize: 28, 
-    color: "#f8f8f8",
-    fontWeight: 'bold',
-    textAlign: 'center',
-    paddingTop: 20,
-    paddingBottom: 20,
+        fontSize: 28, 
+        color: "#f8f8f8",
+        fontWeight: 'bold',
+        textAlign: 'center',
+        paddingTop: 20,
+        paddingBottom: 20,
     }
 })
 export default function Notification(){
+    const notificationList = useSelector(selectNotificationList);
+    if (notificationList.length === 0)
+    {
+        return (
+            <View style={styles.container}>
+                <Header title="Notification"></Header>
+                <View style={styles.container}></View>
+                <Text style={styles.title}>No Notification</Text>
+                <View style={styles.container}></View>
+            </View>
+        )
+    }
     return(
         <View style={styles.container}>
             <Header title="Notification"></Header>
             <View style={styles.container}>
-                <Text style={{color: "#F8F8F8"}} >Notification</Text>
-                
+            <ScrollView style={styles.subcontainer}>
+                {
+                    notificationList.map((item, index) => {
+                        return(
+                            <NotificationItem 
+                            key={index}
+                            notification={item.value}
+                            date={item.date}
+                            onDeleteThisNotification={() => {
+                                store.dispatch(removeFromNotificationListByIndex(index))
+                            }}
+                            ></NotificationItem>
+                        )
+                    }).reverse()
+                }
+            </ScrollView>
             </View>
         </View>
     );
